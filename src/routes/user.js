c66cb0887139f999ce8673c5025992b2a5973007
Minh Anh Pham <minhanhpham@fb.com>
2022-11-11 12:38:21 +0000

    User router now updates & adds show by id if user by id has watched it

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,6 @@
 const {Router} = require("express");
 const userRouter = Router();
-const {User, Show} = require("../models");
+const {Show, User} = require("../models");
 
 // The User Router should GET ALL users from the database using the endpoint /users.
 userRouter.get("/", async (req, res) => {
@@ -39,6 +39,20 @@ userRouter.get("/:id/shows", async (req, res) => {
     }
 })
 
+// The User Router should update and add a show if a user has watched it using an endpoint.
+userRouter.put("/:id/shows/:show", async (req, res) => {
+    try {
+        const userById = await User.findByPk(req.params.id);
+        const showById = await Show.findByPk(req.params.show);
 
+        await showById.setUser(userById);
+        await showById.update({userId: req.params.id});
+
+        res.status(200).json({showById});
+    }
+    catch (error) {
+        res.status(500).json(error);
+    }
+})
 
 module.exports = userRouter;
